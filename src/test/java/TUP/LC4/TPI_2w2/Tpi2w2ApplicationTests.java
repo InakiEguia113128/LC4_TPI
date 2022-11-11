14dbe28736631b107b47e74fe12e826155783203
InakiEguia113128 <90732757+InakiEguia113128@users.noreply.github.com>
2022-11-11 09:54:49 -0300

    Proyecto en blanco

---
+++ src/test/java/TUP/LC4/TPI_2w2/Tpi2w2ApplicationTests.java
@@ -0,0 +1,13 @@
+package TUP.LC4.TPI_2w2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tpi2w2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
